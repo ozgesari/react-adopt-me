6627e0b72e9c6bb6ff90e15818fe4762f8220e16
Ozge Sari <ozgesarri@gmail.com>
2021-03-02 00:04:57 +0000

    dev

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,20 +1,43 @@
-import React, { Component } from 'react';
+import React, {useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
 
-class App extends Component {
-  render() {
+const App = props => {
+ const [personsState, setPersonsState] = useState({
+    persons : [
+      { name : 'Yazmin', age:5},
+      { name : 'Arya', age:3},
+      { name : 'Yamac', age:2}
+  ]
+ });
+//replaces the old one not merges! This is super important!If you want to get the old data the elegant way is using useState('someOtherValue')
+ const [otherState, setOtherState]= useState({otherState: 'some other value'})
+
+ console.log(personsState,otherState);
+
+ const switchNameHandler = () => {
+   setPersonsState({ 
+     persons : [
+     { name : 'Yazmin', age:4},
+     { name : 'Arya', age:3},
+     { name : 'Yamac', age:1}
+ ]})
+ }
+
+
   return (
     <div className="App">
     <h1>Hello, it's me.</h1>
     <p>This is really working!</p>
-    <Person name='Yazmin' age='5' />
-    <Person name='Eray' age='20'>My Hobbies: Codding❤️🧿 </Person>
-    <Person name='Yamac' age='2'/>
+    <button onClick={switchNameHandler}>Switch Name</button>
+    <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+    <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Codding❤️🧿 </Person>
+    <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
     </div>
-  )
- }
-}
+   ) 
+  }
 
 export default App;
+
+
