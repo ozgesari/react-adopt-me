abd214d869ec8c2c9836d7a1e2802156bdf37080
Ozge Sari <ozgesarri@gmail.com>
2021-04-10 12:55:21 +0100

    useEffect

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,32 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { Component } from 'react';
+import classes from './Person.css';
 
 
-import './Person.css';
-
-const StyledDiv = styled.div`
-width: 60%;
-margin: auto;
-border: 1px solid #eee;
-box-shadow: 0 2px 3px #ccc;
-padding: 16px;
-text-align:center; 
-'@media (min-width: 500px)' : {
-    width:'450px'
+class Person extends Component {
+    render() {
+        console.log('[Person.js] rendering...')
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am
+                {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        )
+    }
 }
-`
-
-const person = (props) => {
-    // min-width : equal or greater than
-    // max-width : between 0 and 500px
-    console.log('[Person.js] rendering...')
-    return (
-        <StyledDiv>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-
-            <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
-    )
-}
-
-export default person;
+export default Person;
