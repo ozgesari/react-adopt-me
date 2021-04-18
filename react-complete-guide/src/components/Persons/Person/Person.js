b6be2dd887beebd314e6ff5f765946c4cf69492c
Ozge Sari <ozgesarri@gmail.com>
2021-04-18 21:48:00 +0100

    prop-types

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.css';
 
 
@@ -14,5 +15,12 @@ class Person extends Component {
             </div>
         )
     }
+}
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 }
 export default Person;
