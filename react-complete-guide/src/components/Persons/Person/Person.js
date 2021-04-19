bd8434305a84e23bf55edef9e780cf6f82dbfe23
Ozge Sari <ozgesarri@gmail.com>
2021-04-19 22:59:34 +0100

    ref

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -4,6 +4,16 @@ import classes from './Person.css';
 
 
 class Person extends Component {
+    constructor(props) {
+        super(props)
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.inputElementRef.current.focus()
+    }
+
+
     render() {
         console.log('[Person.js] rendering...')
         return (
@@ -11,7 +21,10 @@ class Person extends Component {
                 <p onClick={this.props.click}>I'm {this.props.name} and I am
                 {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input type="text"
+                    ref={this.inputElementRef}
+                    onChange={this.props.changed}
+                    value={this.props.name} />
             </div>
         )
     }
